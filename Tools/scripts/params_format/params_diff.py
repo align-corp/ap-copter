32203ef5d7e4c9c6f2c15d06ba35b1ae1f6b7056
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2023-12-21 13:06:25 +0800

    Tools: add parameters tool
    Tools: add scripts to manipulate and compare parameter files

---
+++ Tools/scripts/params_format/params_diff.py
@@ -0,0 +1,30 @@
+'''
+tool to compare ArduPilot parameters. An output file is generated with the different parameters.
+'''
+
+import difflib
+import sys
+
+def compare_files(file1, file2, output_file):
+    with open(file1, 'r') as f1, open(file2, 'r') as f2, open(output_file, 'w') as out:
+        lines1 = [line.replace(' ', '') for line in f1.readlines()]
+        lines2 = [line.replace(' ', '') for line in f2.readlines()]
+
+        differ = difflib.Differ()
+        diff = list(differ.compare(lines1, lines2))
+
+        for line in diff:
+            if line.startswith('- '):
+                out.write(line[2:])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python params_diff.py file1.parm file2.parm\nWaring: order matters! The first input file values will be written in the output file")
+        sys.exit(1)
+
+    input_file1 = sys.argv[1]
+    input_file2 = sys.argv[2]
+    output_file = "output.parm"
+
+    compare_files(input_file1, input_file2, output_file)
+    print(f"Differences written to: {output_file}")
---
+++ Tools/scripts/params_format/params_format.py
@@ -0,0 +1,50 @@
+'''
+tool to manipulate ArduPilot parameters file to a default format,
+so they could be easy compared.
+'''
+
+import re
+import sys
+import os
+
+def transform_line(line):
+    # Use regular expressions to match the desired pattern
+    match = re.match(r'^\s*([^,\s]+)\s*([-+]?\d*\.?\d+)\s*$', line)
+    
+    if match:
+        non_numeric_part = match.group(1).strip()
+        numeric_part = match.group(2).strip()
+
+        # Check if the numeric part is an integer
+        if float(numeric_part) == int(float(numeric_part)):
+            numeric_part = int(float(numeric_part))
+        else:
+            numeric_part = float(numeric_part)
+
+        # Combine the matched parts into the desired format
+        transformed_line = f"{non_numeric_part}, {numeric_part}"
+        return transformed_line
+    else:
+        # If the line doesn't match the expected pattern, return it as is
+        return line.strip()
+
+def transform_file(input_file, output_file):
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        for line in infile:
+            transformed_line = transform_line(line)
+            outfile.write(transformed_line + '\n')
+
+def generate_output_filename(input_filename):
+    base_name, extension = os.path.splitext(input_filename)
+    return f"{base_name}_format.parm"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python params_format.py input_file")
+        sys.exit(1)
+
+    input_file_path = sys.argv[1]
+    output_file_path = generate_output_filename(input_file_path)
+
+    transform_file(input_file_path, output_file_path)
+    print(f"Transformation complete. Check the output file: {output_file_path}")
