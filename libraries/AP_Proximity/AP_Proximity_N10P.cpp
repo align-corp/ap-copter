1669487e46d3a4087b17aa83f5b8971afc309876
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-04-05 23:50:14 +0200

    N10P: consider only shortest distance

--- libraries/AP_Proximity/AP_Proximity_N10P.cpp
+++ libraries/AP_Proximity/AP_Proximity_N10P.cpp
@@ -17,12 +17,16 @@
 #include "AP_Proximity_N10P.h"
 
 #if HAL_PROXIMITY_ENABLED
-#include <AP_Math/crc.h>
 #include <ctype.h>
 #include <stdio.h>
-#include <AP_Common/sorting.h>
 
-#include <GCS_MAVLink/GCS.h>
+#define N10P_DEBUGGING 0
+
+#if N10P_DEBUGGING
+#define Debug(fmt, args ...)  do { printf(fmt, ## args); } while (0)
+#else
+#define Debug(fmt, args ...)
+#endif
 
 extern const AP_HAL::HAL& hal;
 
@@ -31,9 +35,6 @@ void AP_Proximity_N10P::update(void)
 {
     if (_uart == nullptr) {
         return;
-    }
-    if (_last_request_sent_ms == 0) {
-        _last_request_sent_ms = AP_HAL::millis();
     }
 
     // process incoming messages
@@ -47,7 +48,7 @@ void AP_Proximity_N10P::update(void)
     }
 }
 
-// check for replies from sensor, returns true if at least one message was processed
+// check for replies from sensor, return true when a message is successfully parsed
 bool AP_Proximity_N10P::read_sensor_data()
 {
     if (_uart == nullptr) {
@@ -58,34 +59,71 @@ bool AP_Proximity_N10P::read_sensor_data()
     bool message_parsed = false;
 
     while (nbytes-- > 0) {
+        // read byte
         uint8_t byte = _uart->read();
-        if (byte == 0xA5 ) {
+
+        // prevent array out of range
+        if (_buffer_count == PROXIMITY_N10P_PACKET_SIZE) {
             _buffer_count = 0;
         }
-            
+
+        // append to buffer
         _buffer[_buffer_count++] = byte;
 
-        if (_buffer_count == PACKET_SIZE){
-            _buffer_count = 0;
-            //check if message has right CRC and update measure STILL NEED TO DECODE PROTOCOL
-            if (crc8_N10(_buffer, PACKET_SIZE-1) == _buffer[PACKET_SIZE-1]){
-                // decode message
-                float angle_start = UINT16_VALUE(_buffer[5],  _buffer[6]) * 0.01f;
-                float angle_stop = UINT16_VALUE(_buffer[105],  _buffer[106]) * 0.01f;
-                float angle_step = (angle_stop - angle_start > 0) ?  (angle_stop - angle_start) / PACKAGE_POINTS : (angle_stop+360 - angle_start) / PACKAGE_POINTS;
-                float angle = angle_start;
-
-                // Calculate and update distances
-                for (uint8_t i = 7; i < 96; i=i+6) {
-                    uint16_t distance = UINT16_VALUE(_buffer[i],  _buffer[i+1]);
-                    angle = (angle > 360) ? (angle - 360) : angle;
-                    update_sector_data(angle, distance);
-                    angle = angle + angle_step;
-                }
-                message_parsed = true;
-            } else {
-                // wrong crc
+        switch (_parse_state)
+        {
+        case WAIT_HEADER1:
+            if (byte == PROXIMITY_N10P_HEADER1 ) {
+                _buffer[0] = byte;
+                _buffer_count = 1;
+                _parse_state = WAIT_HEADER2;
             }
+            break;
+        case WAIT_HEADER2:
+            if (byte == PROXIMITY_N10P_HEADER2 ) {
+                _parse_state = WAIT_LENGTH;
+            } else {
+                _parse_state = WAIT_HEADER1;
+                Debug("Wrong header 2: %d\n", byte);
+            }
+            break;
+        case WAIT_LENGTH:
+            if (byte == PROXIMITY_N10P_LENGTH ) {
+                _parse_state = WAIT_CHECKSUM;
+            } else {
+                _parse_state = WAIT_HEADER1;
+                Debug("Wrong length: %d\n", byte);
+            }
+            break;
+        case WAIT_CHECKSUM:
+            if (_buffer_count == PROXIMITY_N10P_PACKET_SIZE){
+                _buffer_count = 0;
+                //check CRC and update measure
+                if (crc8_N10(_buffer, PROXIMITY_N10P_PACKET_SIZE-1) == _buffer[PROXIMITY_N10P_PACKET_SIZE-1]){
+                    // decode message
+                    float angle_start = UINT16_VALUE(_buffer[5],  _buffer[6]) * 0.01f;
+                    float angle_stop = UINT16_VALUE(_buffer[105],  _buffer[106]) * 0.01f;
+                    float angle_step = (angle_stop - angle_start > 0) ?  (angle_stop - angle_start) / PROXIMITY_N10P_PACKAGE_POINTS : (angle_stop+360 - angle_start) / PROXIMITY_N10P_PACKAGE_POINTS;
+                    float angle = angle_start;
+
+                    // Calculate and update distances
+                    for (uint8_t i = 7; i < 102; i=i+3) {
+                        uint16_t distance = UINT16_VALUE(_buffer[i],  _buffer[i+1]);
+                        angle = (angle > 360) ? (angle - 360) : angle;
+                        update_sector_data(angle, distance);
+                        angle = angle + angle_step;
+                    }
+                    message_parsed = true;
+                } else {
+                    // wrong crc
+                    _parse_state = WAIT_HEADER1;
+                    Debug("Wrong checksum: %d\n", _buffer[PROXIMITY_N10P_PACKET_SIZE-1]);
+                }
+            }
+            break;
+
+        default:
+            break;
         }
     }
     return message_parsed; 
@@ -94,10 +132,7 @@ bool AP_Proximity_N10P::read_sensor_data()
 // process reply
 void AP_Proximity_N10P::update_sector_data(float angle_deg, uint16_t distance_mm)
 {
-    // Median filter every MEDIAN_DEG = 20
-    if (distances_count > DIST_SIZE-1) {
-        distances_count = 0;
-    }
+    // convert angles from [0 360] to [-22.5 337.5]
     if (angle_deg > 337.5) {
         angle_deg -= 360;
     }
@@ -105,57 +140,54 @@ void AP_Proximity_N10P::update_sector_data(float angle_deg, uint16_t distance_mm
 
     // understand in which sector we are
     for (uint8_t i=0; i<8; i++) {
-        if ((angle_deg > -21 + i*45) && (angle_deg < 21 + i*45)) {
+        if ((angle_deg > -22 + i*45) && (angle_deg < 22 + i*45)) {
             sector = i;
         }
     }
-
-    // static variable for last sector sent
-    static uint8_t last_sector = sector;
 
     // abort if the angle is in the middle of two sectors
     if (sector == UINT8_MAX) {
         return;
     }
 
+    // static variable for last sector sent
+    static uint8_t last_sector = sector;
+    static uint16_t shortest_distance_mm = distance_max()*1000;
+    static uint16_t count = 0;
+
     if (sector == last_sector ) {
-        // update sector distances vector
-        distances[distances_count++] = distance_mm;
-    } else if ((sector == last_sector + 1) || ((sector == 0) && (last_sector == 7))) {
-        // moved to next sector, push the sector distance
-        insertion_sort_uint16(distances,distances_count);
-        uint32_t filtered_distance_mm = 0;
-        uint8_t filtered_distance_count = 0;
-
-        // Average minimum distances
-        for (uint8_t i = 0; i < distances_count*0.2; i++) {
-            filtered_distance_mm += distances[i];
-            filtered_distance_count++;
+        // find shortest distance
+        count++;
+        if (distance_mm < shortest_distance_mm && distance_mm > distance_min()*1000) {
+            shortest_distance_mm = distance_mm;
         }
-        filtered_distance_mm /= filtered_distance_count;
+    } else {
+        //Just consider the shortest distance (skip the 3 shortest distance for filtering)
         float angle_deg_sector = last_sector * 45.0f;
-
-        // update count variables
-        distances_count = 0;
-        distances[distances_count++] = distance_mm;
-        last_sector = sector;
 
         // Get location on 3-D boundary based on angle to the object
         const AP_Proximity_Boundary_3D::Face face = frontend.boundary.get_face(angle_deg_sector);
         //check for target too far, target too close and sensor not connected
-        const bool valid = (filtered_distance_mm < distance_max()*1000) && (filtered_distance_mm > distance_min()*1000);
-        if (valid && !ignore_reading(angle_deg_sector, filtered_distance_mm * 0.001f, false)) {
-            frontend.boundary.set_face_attributes(face, angle_deg_sector, filtered_distance_mm * 0.001f, state.instance);
+        const bool valid = (shortest_distance_mm < distance_max()*1000) && (shortest_distance_mm > distance_min()*1000);
+        Debug("Shortest distance: %d, angle: %f, count: %d\n", shortest_distance_mm, angle_deg_sector, count);
+        count = 0;
+        if (valid && !ignore_reading(angle_deg_sector, shortest_distance_mm * 0.001f, false)) {
+            frontend.boundary.set_face_attributes(face, angle_deg_sector, shortest_distance_mm * 0.001f, state.instance);
             // update OA database
-            database_push(angle_deg_sector, filtered_distance_mm * 0.001f);
+            database_push(angle_deg_sector, shortest_distance_mm * 0.001f);
         } else {
             frontend.boundary.reset_face(face, state.instance);
         }
         _last_distance_received_ms = AP_HAL::millis();
-    } else {
-        // update count variables
-        distances_count = 0;
-        distances[distances_count++] = distance_mm;
+
+        // reset shortest_distance_mm, consider also the first new sector distance
+        if (distance_mm > distance_min()*1000) {
+            shortest_distance_mm = distance_mm;
+        } else {
+            shortest_distance_mm = distance_max()*1000;
+        }
+
+        // reset last_sector
         last_sector = sector;
     }
 }
--- libraries/AP_Proximity/AP_Proximity_N10P.h
+++ libraries/AP_Proximity/AP_Proximity_N10P.h
@@ -5,11 +5,14 @@
 #if HAL_PROXIMITY_ENABLED
 #include "AP_Proximity_Backend_Serial.h"
 
-#define PROXIMITY_N10P_TIMEOUT_MS            300    // requests timeout after 0.3 seconds
-#define PACKET_SIZE                          108
-#define	PACKAGE_POINTS                       16
-#define MEDIAN_DEG                           20
-#define DIST_SIZE                            120
+#define PROXIMITY_N10P_TIMEOUT_MS       500
+#define PROXIMITY_N10P_PACKET_SIZE      108
+#define	PROXIMITY_N10P_PACKAGE_POINTS   32
+
+// packet definition
+#define PROXIMITY_N10P_HEADER1     0xA5
+#define PROXIMITY_N10P_HEADER2     0x5A
+#define PROXIMITY_N10P_LENGTH      0x6C
 
 class AP_Proximity_N10P : public AP_Proximity_Backend_Serial {
 
@@ -31,15 +34,17 @@ private:
     void update_sector_data(float angle_deg, uint16_t distance_cm);
     uint8_t crc8_N10(const uint8_t *p, uint8_t len);
 
-    // reply related variables
-    uint8_t _buffer[PACKET_SIZE]; // buffer where to store data from serial
-    uint8_t _buffer_count = 0;
-    uint16_t distances[DIST_SIZE];
-    uint16_t distances_count = 0;
+    enum N10P_state {
+        WAIT_HEADER1 = 0,
+        WAIT_HEADER2,
+        WAIT_LENGTH,
+        WAIT_CHECKSUM
+    };
 
-    // request related variables
-    uint32_t _last_distance_received_ms;    // system time of last distance measurement received from sensor
-    uint32_t _last_request_sent_ms;         // system time of last command set
+    uint8_t _parse_state = WAIT_HEADER1;
+    uint8_t _buffer[PROXIMITY_N10P_PACKET_SIZE]; // buffer where to store data from serial
+    uint8_t _buffer_count = 0;
+    uint32_t _last_distance_received_ms; // system time of last distance measurement received from sensor
 };
 
 #endif // HAL_PROXIMITY_ENABLED
