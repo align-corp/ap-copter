3ab23e5cf31f57e3e6a6e2cb54429a07d68d2b8c
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-04-19 16:00:46 +0200

    version to 1.0.7

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -20,7 +20,7 @@
 // Align version 
 #define MIDDLE_MAJOR 1
 #define MIDDLE_MINOR 0
-#define MIDDLE_PATCH 6
+#define MIDDLE_PATCH 7
 #define MIDDLE_TYPE FIRMWARE_VERSION_TYPE_DEV
 
 #include <AP_Common/AP_FWVersionDefine.h>
