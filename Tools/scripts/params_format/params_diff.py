88e8a3a323843435b803cbefdf8223e680e3f617
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-10-11 18:22:05 +0800

    Tools: changed params_diff output name

--- Tools/scripts/params_format/params_diff.py
+++ Tools/scripts/params_format/params_diff.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     input_file1 = sys.argv[1]
     input_file2 = sys.argv[2]
-    output_file = "Align_diff.param"
+    output_file = "defaults.parm"
 
     compare_files(input_file1, input_file2, output_file)
     print(f"Differences written to: {output_file}")
