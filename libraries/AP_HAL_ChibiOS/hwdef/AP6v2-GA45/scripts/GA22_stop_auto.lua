d7f756975b86faab515d14ce6074c8dc154e5105
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-10-28 21:03:39 +0800

    hwdef: update GA45 scripts

--- libraries/AP_HAL_ChibiOS/hwdef/AP6v2-GA45/scripts/GA22_stop_auto.lua
+++ libraries/AP_HAL_ChibiOS/hwdef/AP6v2-GA45/scripts/GA22_stop_auto.lua
@@ -1,17 +1,10 @@
 local AUTO_MODE = 3
 local MANUAL_MODE = 0
-local count = 0
 function update()
 if vehicle:get_mode() ~= AUTO_MODE then
 return update, 1000
 end
 if gps:status(0) < gps.GPS_OK_FIX_3D_RTK_FLOAT or gps:status(1) < gps.GPS_OK_FIX_3D_RTK_FLOAT then
-count = count + 1
-else
-count = 0
-end
-if count > 5 then
-count = 0
 vehicle:set_mode(MANUAL_MODE)
 arming:disarm()
 gcs:send_text('0', "Auto mode need RTK")
