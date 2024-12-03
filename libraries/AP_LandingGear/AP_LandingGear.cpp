b5aee61592bafae1470437cb05da6076d0246984
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-12-03 11:21:55 +0100

    lower hard coded distance

--- libraries/AP_LandingGear/AP_LandingGear.cpp
+++ libraries/AP_LandingGear/AP_LandingGear.cpp
@@ -297,7 +297,8 @@ void AP_LandingGear::update(float height_above_ground_m)
             alt_m >= _retract_alt &&
             _last_height_above_ground < _retract_alt) {
             // prevent lidar noise near ground to trigger retract
-            if (_last_height_above_ground <= 2 && alt_m > 40) {
+            //TODO: remove hard coded distances
+            if (_last_height_above_ground <= 2 && alt_m > 20) {
                 return;
             }
             retract();
